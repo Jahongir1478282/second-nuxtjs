f66ff2b3a39f3e34519c0a6cc959496f6885f663
Jakhongir <61061199+Jahongir1478282@users.noreply.github.com>
2023-01-05 09:35:17 +0500

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>Hello World!</div>
+  <button @click="increment">+</button>
+  <p>{{ count }}</p>
+  <button @click="decrement">-</button>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+    sit porro neque quis vero voluptatibus sequi nisi id ullam iure,
+    rerum velit, nihil dolores doloribus ab totam voluptas optio
+    voluptatum!
+  </p>
+</template>
+
+<script setup>
+//  Composition API
+const count = ref(0);
+function increment() {
+  count.value++;
+}
+function decrement() {
+  count.value--;
+}
+</script>
+<!-- <script>
+// Optoins API
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    },
+  },
+};
+</script> -->
